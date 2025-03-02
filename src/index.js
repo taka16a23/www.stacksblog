0f9ab09fec7580fdf3c60a0eb8bc7cff2d217783
a23 <takahiroatsumi0517@gmail.com>
2025-03-02 19:32:52 +0900

    Refactoring

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React, { Suspense, lazy } from "react";
 
 import ReactDOM from "react-dom/client";
 
+import "assets/scss/style.min.css";
 import Spinner from "components/spinner/Spinner";
 import axiosSetup from "helpers/interceptors";
-import "assets/scss/style.min.css";
-
-// setup axios
-axiosSetup()
-
 const App = lazy(() => new Promise((resolve) => {
   setTimeout(() => resolve(import("./app")), 0);
 }));
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// setup axios
+axiosSetup()
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <Suspense fallback={<Spinner />}>
-    <App />
+  <Suspense fallback={<Spinner/>}>
+    <App/>
   </Suspense>
 )
